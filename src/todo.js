4ee13c457ca577f3bf782b7a8a1feb78bf6be10b
Shalama <shalamafadillaa@gmail.com>
2025-10-29 14:59:58 +0700

    Delete parseDate function

--- src/todo.js
+++ src/todo.js
@@ -1,11 +1,8 @@
-// Import date-fns library
-import { isValid, parse } from "date-fns";
-
 // Create a class to create a todo object
 export default class Todo {
     constructor(title, dueDate) {
         this.title = title;
-        this.dueDate = parseDate(dueDate);
+        this.dueDate = dueDate;
         this.desc = "";
         this.priority = "";
         this.note = "";
@@ -42,10 +39,4 @@ export default class Todo {
     set setProject(project) {
         this.project = project;
     }
-}
-
-// Create a function to parse the dueDate to date object
-function parseDate(dueDate) {
-    const parsedDate = parse(dueDate, "yyyy/MM/dd HH:mm", new Date());
-    return isValid(parsedDate) ? parsedDate : null;
 }
