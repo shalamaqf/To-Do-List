049f2b1ce098d3d77468f36b89ad938d9b272fc5
Shalama <shalamafadillaa@gmail.com>
2025-10-29 13:54:13 +0700

    Move the module to projectManager.js

--- src/projectManager.js
+++ src/projectManager.js
@@ -1,2 +1,16 @@
 // Import class project
 import Project from './project.js';
+
+// Create an array to store projects
+export let projectList = [];
+
+// Create a function to create a project object
+function createProject(title) {
+    return new Project(title);
+}
+
+// Create a function to add a project o projectList array
+export function addProject(title) {
+    const newProject = createProject(title);
+    projectList.push(newProject);
+}
