a825da643fc21e244220ce50de46b0467f8d0764
Shalama <shalamafadillaa@gmail.com>
2025-10-30 14:52:13 +0700

    Create a function to stringify the project

---
+++ src/storage.js
@@ -0,0 +1,7 @@
+// Create a function to turn project and todo object to string 
+function stringifyProject(project) {
+    // Stringify the project
+    const projectStr = JSON.stringify(project);
+    
+    return projectStr;
+}
