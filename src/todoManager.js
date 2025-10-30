b5278ec219243e545efc090a7a7f504a1163035e
Shalama <shalamafadillaa@gmail.com>
2025-10-30 10:47:13 +0700

    Craete a function create a todo object

--- src/todoManager.js
+++ src/todoManager.js
@@ -2,3 +2,7 @@
 import {} from './projectManager.js';
 import Todo from './todo.js';
 
+// Create a function to create a todo object
+export function createTodo(title, dueDate) {
+    return new Todo(title, dueDate);
+}
