13297ba5334584889cb4fb44434f80f4af6e6f4a
Shalama <shalamafadillaa@gmail.com>
2025-11-03 13:18:07 +0700

    Add button elements in project title container

--- src/projectDOM.js
+++ src/projectDOM.js
@@ -3,14 +3,26 @@ export function renderProjectTitle(project) {
     const projectListContainer = document.querySelector("#projectList-container");
     const projectTitleDiv = document.createElement("div");
     const projectTitle = document.createElement("h4");
+    const projectBtnContainer = document.createElement("div");
+    const deleteProjectBtn = document.createElement("button");
+    const renameProjectBtn = document.createElement("button");
 
     projectTitleDiv.classList.add("projectTitle-container");
     projectTitle.classList.add("projectTitle");
+    projectBtnContainer.className = "projectBtn-container";
+    deleteProjectBtn.className = "deleteProject-button";
+    renameProjectBtn.className = "renameProject-button";
 
     projectTitle.textContent = project.title;
+    deleteProjectBtn.textContent = "-";
+    renameProjectBtn.textContent = "✎";
 
+    projectBtnContainer.appendChild(renameProjectBtn);
+    projectBtnContainer.appendChild(deleteProjectBtn);
     projectTitleDiv.appendChild(projectTitle);
+    projectTitleDiv.appendChild(projectBtnContainer);
     projectListContainer.appendChild(projectTitleDiv);
     
     return projectTitleDiv;
 }
+
