6bd12eceb36d0c162ca0a6dcbcaab551e087bb74
Shalama <shalamafadillaa@gmail.com>
2025-11-14 10:23:10 +0700

    Create an init function for index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
+import { initProjectDOM } from "./projectDOM";
+import { inbox } from "./projectManager";
 import "./style.css";
+import { initTodoDOM } from "./todoDOM";
+
+initProjectDOM(inbox);
+initTodoDOM();
