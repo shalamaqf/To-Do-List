42e8878c5e38f3d9b8f7f97d426e458ba8ebbb8e
Shalama <shalamafadillaa@gmail.com>
2025-11-17 14:12:08 +0700

    Renew the logic for initProjectDOM

--- src/projectDOM.js
+++ src/projectDOM.js
@@ -316,9 +316,18 @@ function setupProjectButton(projectBtn, project) {
 }
 
 // Create a function to initiallize the project DOM
-export function initProjectDOM(inbox) {
-    storeCurrentProject(inbox);
-    renderDefaultProjectTitle(inbox);
+export function initProjectDOM(defaultProject) {
+    const savedProjectTitle = localStorage.getItem("CURRENT_PROJECT");
+    const projectToRender = projectList.find(p => p.title === savedProjectTitle) || defaultProject;
+
+    storeCurrentProject(projectToRender);
+
+    if (projectToRender.title === "Inbox") {
+        renderDefaultProjectTitle(projectToRender); // For inbox
+    } else {
+        renderProjectTitle(projectToRender);        // For other projects
+    }
+
     setupAddProject();
     setupFormProject();
 }
