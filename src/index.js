ae73bfd6152a88830528a18a39181da4228b1293
Shalama <shalamafadillaa@gmail.com>
2025-11-17 14:24:34 +0700

    Create an init function to run the program

--- src/index.js
+++ src/index.js
@@ -1,7 +1,29 @@
-import { initProjectDOM } from "./projectDOM.js";
-import { inbox } from "./projectManager.js";
-import "./style.css";
-import { initTodoDOM } from "./todoDOM.js";
+import { initProjectDOM } from "./projectDOM";
+import { createDefaultProject, projectList } from "./projectManager";
+import { dataToObject } from "./storage";
+import { initTodoDOM } from "./todoDOM";
 
-initProjectDOM(inbox);
-initTodoDOM();
+// Create a function as init function for the program
+function init() {
+    // Create a variable to store the default project
+    let defaultProject;
+
+    // Check project list's length
+    if (projectList.length === 0) {
+        defaultProject = createDefaultProject();
+    }
+
+    // Load raw data, parse and deserialzied it
+    dataToObject();
+
+    // If defaultProject wasn't created above (projectList not empty), fallback
+    if (!defaultProject) {
+        defaultProject = projectList[0]; // just pick the first project
+    }
+
+    // Render the project dom section
+    initProjectDOM(defaultProject);
+
+    // Render the todo's section
+    initTodoDOM();
+}
